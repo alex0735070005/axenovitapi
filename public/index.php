7a712bce1921a5f2acb42b3f9bb84290b3f351e6
alex0735070005 <alex0735070005@gmail.com>
2019-09-07 16:21:58 +0300

    v2

--- public/index.php
+++ public/index.php
@@ -6,11 +6,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
 
-    Debug::enable();
-}
+Debug::enable();
+
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
