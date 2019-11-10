f2bfe4303efdd47416a851bfb2371e873ee255b4
alex0735070005 <alex0735070005@gmail.com>
2019-11-10 06:37:22 +0200

    add-htaccess

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,18 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+    public function __construct(SessionInterface $session)
+    {
+        header('Access-Control-Allow-Origin: *');
+        $this->session = $session;
+                
+        include_once __DIR__ . '/../../config/data.php';
+        
+        $this->posts = $this->session->get('posts');
+        
+        if(!isset($this->posts) || empty($this->posts)) {
+            $this->session->set('posts', $posts);
+            $this->posts = $posts;
+        }
+         
+    }
+    
     /**
      * @Route("/posts", name="posts", methods={"GET"})
      */
     public function list()
-    {
-        include_once __DIR__ . '/../../config/data.php';
-
-        return $this->json($users);
+    { 
+       header('Access-Control-Allow-Origin: *');
+        return $this->json($this->posts);
     }
 
     /**
@@ -22,17 +38,16 @@ class BlogController extends AbstractController
      */
     public function getPost($id)
     {
-        include_once __DIR__ . '/../../config/data.php';
+        header('Access-Control-Allow-Origin: *');
+        $result = [];
 
-        $post = [];
-
-        foreach ($users as $user) {
-            if($user['id'] === $id){
-                $post = $user;
+        foreach ($this->posts as $post) {
+            if($post['id'] === $id){
+                $result = $post;
             }
          }
 
-        return $this->json($post);
+        return $this->json($result);
     }
 
     /**
@@ -40,6 +55,7 @@ class BlogController extends AbstractController
      */
     public function create()
     {
+        header('Access-Control-Allow-Origin: *');
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/create',
@@ -47,14 +63,38 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/posts/{id}", name="update", methods={"PUT"})
+     * @Route("/posts/update/{id}", name="update", methods={"POST"})
      */
-    public function update()
+    public function update($id)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/update',
-        ]);
+        header('Access-Control-Allow-Origin: *');
+        
+        $data = json_decode(file_get_contents('php://input'), true);
+      
+        if(!isset($data) || empty($data)) {die('404');}
+
+        $result = [];
+       
+        foreach ($this->posts as $key => $post) {
+            if($post['id'] === $id){
+                 
+               $this->posts[$key] = $data;
+               // var_dump($post); die();
+               $this->posts[$key] = [
+                   "id"=> $post['id'],
+                   "title"=> $post['title'],
+                   "user"=> $post['user'],
+                   "description"=> isset($data['description']) ? $data['description'] : $post['description'],
+                   "data_create"=> $post['data_create'],
+                   "data_update"=>  date("Y-m-d H:i:s"),
+                   "likes"=> isset($data['likes']) ? $data['likes'] : $post['likes'],
+                   "favorite"=> isset($data['favorite']) ? $data['favorite'] : $post['favorite'],
+               ];
+               $result = $this->posts[$key];
+               $this->session->set('posts', $this->posts);
+            }
+         }
+        return $this->json($result);
     }
 
     /**
@@ -62,9 +102,18 @@ class BlogController extends AbstractController
      */
     public function delete()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/delete',
-        ]);
+        header('Access-Control-Allow-Origin: *');
+        $result = [];
+       
+        foreach ($this->posts as $key => $post) {
+             if($post['id'] === $id){                 
+                unset($this->posts[$key]);
+               
+                $result = ["id" => $id];
+                $this->session->set('posts', $this->posts);
+             }
+        }        
+       
+        return $this->json($result);
     }
 }
